6f950999c538c709756b01ea3fb39c837139f76f
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2024-02-03 21:44:05 -0500

    Update 2 Red Flags.py

--- pages/2 Red Flags.py
+++ pages/2 Red Flags.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Red Flags')
 st.header("Vaccine Reactions")
-st.warning("Rabies Vaccine Reaction: 12/9/2023 /n Kaia had a reaction to her rabies vaccine. After 20 minutes she collapsed and
+st.warning("Rabies Vaccine Reaction: 12/9/2023 '\n Kaia had a reaction to her rabies vaccine. After 20 minutes she collapsed and
 had difficulty breathing. She was rushed to the crash table and supplemental oxygen was given.")
 
 # st.header("Drug Reactions")
