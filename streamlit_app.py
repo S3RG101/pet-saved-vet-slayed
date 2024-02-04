4b7d94ee842e92ba62d72edbb14d8f0aeb0b74cc
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2024-02-03 20:59:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,28 +3,16 @@ from streamlit_apexjs import st_apexcharts
 
 st.set_page_config(page_title='Pet Saved, Vet Slayed®')
 
-def mainpage():
-  st.title('Pet Saved, Vet Slayed®')
+st.title('Pet Saved, Vet Slayed®')
 
-  click = None
+click = None
 
-  if click is None:
-    file = st.file_uploader("Choose Medical History in PDF", type='pdf', key='file_uploader')
+if click is None:
+  file = st.file_uploader("Choose Medical History in PDF", type='pdf', key='file_uploader')
 
-  if not file is None:
-    click = st.button('Proceed')
-    if click:
+if not file is None:
+  click = st.button('Proceed')
+  if click:
 
-      with st.sidebar:
-        pat_sum = st.button('Patient Summary')
-        if pat_sum:
-          st.session_state.STATE = 1
-          st.experimental_rerun()
-        red_flag = st.button('Red Flags')
-        if red_flag:
-          st.session_state.STATE = 2
-          st.experimental_rerun()
-        hos_stats = st.button('Hospital Stats')
-        if hos_stats:
-          st.session_state.STATE = 3
-          st.experimental_rerun()
+    with st.sidebar:
+      print('Hello')
