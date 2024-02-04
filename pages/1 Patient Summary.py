0300d4a273f5aabcc7288f74b7b4049391cd755b
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2024-02-03 21:24:23 -0500

    Update 1 Patient Summary.py

--- pages/1 Patient Summary.py
+++ pages/1 Patient Summary.py
@@ -5,6 +5,14 @@ st.title('Patient Summary')
 col1, col2= st.columns(2)
 
 with col1:
+  st.subheader("Name")
+  cont1 = st.container(border=True)
+  cont1.write("Esteban")
+  
+  st.subheader("Breed")
+  cont1 = st.container(border=True)
+  cont1.write("Shiba Inu")
+  
   st.subheader("Active Concerns")
   cont1 = st.container(border=True)
   cont1.write("Spay 01-Aug-2019")
@@ -13,7 +21,21 @@ with col1:
   cont1 = st.container(border=True)
   cont1.write("Wellness/Preventive care 31-Aug-2017")
 
+  
+
 with col2:
+  st.subheader("Age")
+  cont1 = st.container(border=True)
+  cont1.write("2 years, 11 months")
+
+  st.subheader("Sex")
+  cont1 = st.container(border=True)
+  cont1.write("Female/Spayed")
+
+  st.subheader("Weight")
+  cont1 = st.container(border=True)
+  cont1.write("19.1 lb")
+  
   st.subheader("Medications")
   cont1 = st.container(border=True)
   cont1.write("Sentinel 11-25lb/5-11kg Green 6pk")
