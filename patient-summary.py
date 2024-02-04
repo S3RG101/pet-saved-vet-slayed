dfbb4f8c254412ca8f5dc3b32509ee43843a94ab
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2024-02-03 20:50:15 -0500

    Update and rename pages/page1.py to patient-summary.py

---
+++ patient-summary.py
@@ -0,0 +1,20 @@
+
+st.title('Patient Summary')
+
+col1, col2= st.columns(2)
+
+with col1:
+  st.subheader("Active Concerns")
+  cont1 = st.container(border=True)
+  cont1.write("Spay 01-Aug-2019")
+  cont1 = st.container(border=True)
+  cont1.write("Otitis externa 28-Oct-2018")
+  cont1 = st.container(border=True)
+  cont1.write("Wellness/Preventive care 31-Aug-2017")
+
+with col2:
+  st.subheader("Medications")
+  cont1 = st.container(border=True)
+  cont1.write("Sentinel 11-25lb/5-11kg Green 6pk")
+  cont1 = st.container(border=True)
+  cont1.write("Rovera (Carprofen) 75mg Cw Tab")
