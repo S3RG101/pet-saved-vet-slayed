2f2b135e132b3771a56b3b399a6db9c3bb393435
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2024-02-04 08:05:26 -0500

    Update 2 Red Flags.py

--- pages/2 Red Flags.py
+++ pages/2 Red Flags.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title('Red Flags')
 st.header("Vaccine Reactions")
 st.warning('''**Rabies Vaccine Reaction** (12/9/2023)  
-          Kaia had a reaction to her rabies vaccine. After 20 minutes she collapsed and had difficulty breathing. She was rushed to the crash table and supplemental oxygen was given.''')
+          Stephana had a reaction to her rabies vaccine. After 20 minutes she collapsed and had difficulty breathing. She was rushed to the crash table and supplemental oxygen was given.''')
 
 # st.header("Drug Reactions")
 
 st.header("Antimicrobial Resistance")
 st.error('''**Bacterial Cystitis**  
-          Kaia had bacterial cystitis that was treated with amoxicillin for 7 days but it did not resolve. After doing a culture and sensitivity, it came back as resistant to amoxicillin.''')
+          Stephana had bacterial cystitis that was treated with amoxicillin for 7 days but it did not resolve. After doing a culture and sensitivity, it came back as resistant to amoxicillin.''')
